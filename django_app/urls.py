ba975d6f47e204487e82abee0285328f05ad3ca7
Vadim <120014140+Yomorad@users.noreply.github.com>
2024-05-06 23:21:27 +0500

    just_fix_name_dir

--- django_app/urls.py
+++ django_app/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for django_study project.
+URL configuration for django_app project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
-from django_study import settings
+from django_app import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
